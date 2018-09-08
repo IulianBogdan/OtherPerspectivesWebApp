e0c7c70abcf8c18616566d7eca17e81d2ebbf230
tibbiustin <tibe.kinda@yahoo.ro>
2018-09-08 16:37:01 +0200

    new models

---
+++ Models/Order.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OtherPerspectivesWebApp.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey("Product")]
+        public int ProductId { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        public string Comment { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OtherPerspectivesWebApp.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+    }
+}
